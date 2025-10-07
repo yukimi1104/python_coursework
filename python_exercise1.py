7c54d3a9cc7c54ecfe6be871651476dd9fbb128a
yukimi1104 <yukimi1999@outlook.com>
2025-10-07 11:34:10 +0200

    Update python_exercise1.py

--- python_exercise1.py
+++ python_exercise1.py
@@ -109,6 +109,8 @@ print(count)
 positions = [i for i in range(len(my_string)) if my_string.startswith("GG", i)]
 print("Positions of GG:", positions) #[expression for variable in iterable if condition]
 import re
+for match in re.finditer("GG",my_string)
+    print(match.start())
 matches = re.finditer(pattern, seq)
 positions = [match.start() for match in re.finditer("GG", seq)]
 print(positions)
@@ -296,6 +298,7 @@ while len(primes) < 1000:
     num += 1
 for prime in primes:
     print(prime, end=' ')
+
 
 
 
