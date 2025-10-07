a5c7338a9b160f9b9f838071ce2dc048cd548b87
yukimi1104 <yukimi1999@outlook.com>
2025-10-07 10:57:01 +0200

    Update python_exercise1.py

--- python_exercise1.py
+++ python_exercise1.py
@@ -276,6 +276,25 @@ print("First 20 primes:", primes[:20])
 # Uncomment below to print all
 # print(primes)
 
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+primes = []
+num = 2  # start checking from 2
+
+while len(primes) < 1000:
+    if is_prime(num):
+        primes.append(num)
+    num += 1
+for prime in primes:
+    print(prime, end=' ')
+
+
+
 
 
 
